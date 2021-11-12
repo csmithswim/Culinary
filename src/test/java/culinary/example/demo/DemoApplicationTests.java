d37d435061d7e12626911ef23bda657d01fb0880
Cullen S <cullensmi@gmail.com>
2021-11-12 17:23:07 -0500

    first commit

---
+++ src/test/java/culinary/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package culinary.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
