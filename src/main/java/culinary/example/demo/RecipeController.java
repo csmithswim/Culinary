09b395190e982849afaf9ce024a1d2b2dd3cc53d
Cullen S <cullensmi@gmail.com>
2021-11-17 15:41:56 -0500

    added custom exception class to catch any null exceptions with recipe objects passed to GET request and solved second stage

--- src/main/java/culinary/example/demo/RecipeController.java
+++ src/main/java/culinary/example/demo/RecipeController.java
@@ -1,25 +1,37 @@
 package culinary.example.demo;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class RecipeController {
 
+
     List<Recipe> recipes = new ArrayList<>();
 
-    @PostMapping("/api/recipe")
-    public int postRecipe(@RequestBody Recipe recipe) {
+    @PostMapping("/api/recipe/new")
+    public Map<String, Integer> postRecipe(@RequestBody Recipe recipe) {
         recipes.add(recipe);
-        return recipes.size() - 1;
+        Map<String, Integer> id = new HashMap<>();
+        id.put("id", recipes.size() - 1);
+        return id;
     }
 
-    @GetMapping("/api/{id}")
-    Recipe getRecipes(@PathVariable int id) {
-        return recipes.get(id - 1);
+    @GetMapping("/api/recipe/{id}")
+    public Recipe getRecipes(@PathVariable int id) {
+        if (id > recipes.size()) {
+            throw new RecipeNotFoundException("recipe not found for id = " + id);
+        }
+        return recipes.get(id);
+
     }
 
 
 }
+
---
+++ src/main/java/culinary/example/demo/RecipeNotFoundException.java
@@ -0,0 +1,13 @@
+package culinary.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class RecipeNotFoundException extends RuntimeException{
+
+    public RecipeNotFoundException(String cause) {
+        super(cause);
+    }
+}
